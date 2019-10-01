0b394eebf765308a71eb2f41fce5401b4ffaabc3
Marcos <marcosvalens@gmail.com>
2019-10-01 16:55:42 +0200

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        Data data =  new Data();
+        Data data =  new Data(01,10,2019);
         data.setAny(2019);
         System.out.println(data.getAny());
     }
